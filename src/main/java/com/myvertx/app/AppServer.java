56dacd69ea1f68c4abdfe905f2c907a868497696
Condere Jerome <jerome.condere@gmail.com>
2017-02-14 23:56:48 +0100

    default start for client and server

--- src/main/java/com/myvertx/app/AppServer.java
+++ src/main/java/com/myvertx/app/AppServer.java
@@ -3,5 +3,13 @@ package com.myvertx.app;
 import io.vertx.core.AbstractVerticle;
 
 public class AppServer extends AbstractVerticle {
-
+	@Override
+    public void start() throws Exception {
+    
+        vertx.createHttpServer() 
+        .requestHandler(req -> req.response() 
+            .putHeader("content-type", "text/html") 
+            .end("<html><body><h1>AppB2</h1></body></html>"))
+        .listen(8080);
+    }
 }
