5804a828e314a0a7fbc34902281723c706fbacbc
Condere Jerome <jerome.condere@gmail.com>
2017-02-26 19:18:25 +0100

    begin mongo client

--- src/main/java/com/myvertx/app/App.java
+++ src/main/java/com/myvertx/app/App.java
@@ -1,12 +1,26 @@
 package com.myvertx.app;
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.eventbus.EventBus;
 
 public class App extends AbstractVerticle
 {
 	@Override
 	public void start() throws Exception 
 	{
-	    
+		/*
+		 * avec l'event bus l'application est plus scalable
+		 * de sorte que on peut l'etendre sur plusieurs serveurs
+		 * Les macros ne communiquent pas directement avec la base de
+		 * données ils passent par les services de l'App.
+		 */
+		System.out.println("Start of Container");
+		final EventBus bus = vertx.eventBus();
+		 vertx.eventBus().consumer("db.import", message -> {
+	           System.out.println("import du fichier: "+message.body());
+	     });
+		 vertx.eventBus().consumer("launch.service", message -> {
+	           System.out.println("lancement du service: "+message.body());
+	     });
 	}
 
 	@Override
--- src/main/java/com/myvertx/app/AppDB.java
+++ src/main/java/com/myvertx/app/AppDB.java
@@ -1,13 +1,37 @@
 package com.myvertx.app;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.mongo.MongoClient;
 
 public class AppDB extends AbstractVerticle {
-	
+	MongoClient mongoClient;
+	private static int MONGO_PORT = 12345;
+	private static int HTTP_PORT = 28017;
+	private static String uri = "mongodb://localhost:27017";
+	private static String DB_NAME = "mongodb://localhost:27017";
 	@Override
 	public void start() throws Exception 
 	{
-		
+	    JsonObject mongoconfig = new JsonObject()
+	            .put("connection_string", uri)
+	            .put("db_name", "db");
+		mongoClient = MongoClient.createShared(vertx, mongoconfig);
+		JsonObject document = new JsonObject().put("title", "The Hobbit");
+
+		mongoClient.save("books", document, res -> {
+
+		  if (res.succeeded()) {
+
+		    String id = res.result();
+		    System.out.println("Saved book with id " + id);
+
+		  } else {
+		    res.cause().printStackTrace();
+		  }
+
+		});
 	}
 	
 	@Override
