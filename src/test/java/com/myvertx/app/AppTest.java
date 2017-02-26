4e64e09e9287b97342386eac6fb120a85376ed85
Condere Jerome <jerome.condere@gmail.com>
2017-02-26 17:12:00 +0100

    rm tests

--- src/test/java/com/myvertx/app/AppTest.java
+++ src/test/java/com/myvertx/app/AppTest.java
@@ -30,73 +30,7 @@ public class AppTest
 	public void setUp(TestContext context)
 	{
 	  vertx = Vertx.vertx();
-	  vertx.deployVerticle("com.myvertx.app.AppCSVServer", context.asyncAssertSuccess());
-	  vertx.deployVerticle("com.myvertx.app.AppServer", context.asyncAssertSuccess());
 	}
-	
-
-	// TODO corriger pour le path
-	// fonctionne avec path = iji
-	@Test
-	 public void testServerRessourceAvailable(TestContext context) 
-	 {
-		  //verifie qu'un fichier non existant n'est pas un ressource disponible
-		  Async async = context.async();
-		  HttpClientOptions options = new HttpClientOptions().setDefaultHost("localhost").setDefaultPort(8081);
-		  HttpClient client = vertx.createHttpClient(options); 
-		  client.post("/ressource/availaible", response -> {
-			  response.bodyHandler(body -> {
-					 context.assertEquals("error", body.toJsonObject().getString("message"));
-					 async.complete();
-			  });
-		  }).putHeader("content-type", "application/json")
-		  	.end(new JsonObject().put("path", "cubijk.kh")
-		  						 .encodePrettily());
-		  //verifie qu'un fichier existant est bien une ressource disponible
-		  Async async2 = context.async();
-		  client.post("/ressource/availaible", response -> {
-			  response.bodyHandler(body -> {
-					 context.assertEquals("success", body.toJsonObject().getString("message"));
-					 async2.complete();
-			  });
-		  }).putHeader("content-type", "application/json")
-		  	.end(new JsonObject().put("path", "data/test.txt")
-		  						 .encodePrettily());
-	 }
-	@Test
-	 public void testServerGetAlimentbyId(TestContext context) 
-	 {
-		context.fail();
-	 }
-	@Test
-	 public void testServerGetAllAliments(TestContext context) 
-	 {
-	    context.fail();
-	 }
-	@Test
-	 public void testServerGetAlimentbyName(TestContext context) 
-	 {
-		context.fail();
-	 }
-	@Test
-	 public void testServerGetAlimentGlucide(TestContext context) 
-	 {
-		context.fail();
-	 }	
-	@Test
-	 public void testServerGetAlimentLipide(TestContext context) 
-	 {
-		context.fail();
-	 }	
-	@Test
-	 public void testServerGetAlimentProtide(TestContext context) 
-	 {
-		context.fail();
-	 }	
-	
-	
-	
-	
 	
 	@After
 	public void tearDown(TestContext context) 
